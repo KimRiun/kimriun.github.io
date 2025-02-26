98210f23f3f6c7a12ba225d7cd351ea9ade66846
KimRiun <kimriun27@gmail.com>
2025-02-27 00:56:39 +0900

    feat: history 카드 뒤집기 애니메이션 추가

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -11,33 +11,37 @@ interface HistoryCardProps {
   award?: string;
   image: string;
   backgroundColor: string;
-  backData: { title: string; contents: string[] }[];
+  backContents: { title: string; contents: string[] }[];
 }
 
 export default function HistoryCard({ data }: { data: HistoryCardProps }) {
-  const { year, titleText, content, award, image, backgroundColor, backData } = data;
+  const { year, titleText, content, award, image, backgroundColor, backContents } = data;
   const [showBack, setShowBack] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
   const handleClick = () => setShowBack((prev) => !prev);
 
   return (
-    <Frame $showBack={showBack} $backgroundColor={backgroundColor} onClick={handleClick}>
-      {!showBack ? (
-        <InnerFrame $backgroundColor={backgroundColor}>
+    <CardContainer>
+      <Frame $showBack={showBack} $backgroundColor={backgroundColor} onClick={handleClick}>
+        <InnerFrame
+          $backgroundColor={backgroundColor}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           <Year>{year}</Year>
-          <Title $image={`/images/${image}`}>
+          <Title $image={`/images/${image}`} $isHovered={isHovered}>
             <TitleText>{titleText}</TitleText>
           </Title>
           <Content>{content}</Content>
           {award && <Award>{award}</Award>}
         </InnerFrame>
-      ) : (
-        <BackFrame $image={`/images/${image}`}>
+        <BackFrame $image={`/images/${image}`} $backgroundColor={backgroundColor}>
           <BackContent>
-            {backData.map((item, index) => (
+            {backContents.map(({ title, contents }, index) => (
               <div key={index}>
-                <b>{item.title}</b>
+                <b>{title}</b>
                 <ul>
-                  {item.contents.map((content, contentIndex) => (
+                  {contents.map((content, contentIndex) => (
                     <li key={contentIndex}>{content}</li>
                   ))}
                 </ul>
@@ -45,30 +49,43 @@ export default function HistoryCard({ data }: { data: HistoryCardProps }) {
             ))}
           </BackContent>
         </BackFrame>
-      )}
-    </Frame>
+      </Frame>
+    </CardContainer>
   );
 }
 
+const CardContainer = styled.div`
+  perspective: 1000px; /* 3D 효과를 위한 원근감 */
+`;
+
 const Frame = styled.div<{ $showBack: boolean; $backgroundColor: string }>`
   position: relative;
-  background-color: ${({ $showBack, $backgroundColor }) =>
-    $showBack ? $backgroundColor : 'white'};
-  border-radius: 0.5rem;
   width: 14rem;
   height: 24rem;
   padding: 1rem;
+  background-color: white;
+  border-radius: 0.5rem;
   box-shadow: 0px 10px 10px rgba(59, 45, 38, 0.1);
+  transform-style: preserve-3d;
+  transition: transform 0.6s ease-in-out;
+  transform: ${({ $showBack }) => ($showBack ? 'rotateY(-180deg)' : 'rotateY(0deg)')};
   cursor: pointer;
 `;
 
-const BackFrame = styled.div<{ $image: string }>`
+const BackFrame = styled.div<{ $image: string; $backgroundColor: string }>`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
+  background-color: ${({ $backgroundColor }) => $backgroundColor};
+  border-radius: 0.5rem;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  backface-visibility: hidden;
+  transform: rotateY(-180deg);
 
   &::after {
     content: '';
@@ -86,6 +103,7 @@ const BackFrame = styled.div<{ $image: string }>`
 `;
 
 const BackContent = styled.div`
+  padding: 1rem;
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
@@ -93,14 +111,15 @@ const BackContent = styled.div`
 `;
 
 const InnerFrame = styled.div<{ $backgroundColor: string }>`
+  position: relative;
   width: 100%;
   height: 100%;
+  background-color: ${({ $backgroundColor }) => $backgroundColor};
+  box-shadow: inset 0px 3px 3px rgba(141, 102, 83, 0.1);
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: ${({ $backgroundColor }) => $backgroundColor};
-  box-shadow: inset 0px 3px 3px rgba(141, 102, 83, 0.1);
-  position: relative;
+  backface-visibility: hidden;
 `;
 
 const Year = styled.p`
@@ -112,15 +131,16 @@ const Year = styled.p`
   text-align: center;
 `;
 
-const Title = styled.div<{ $image: string }>`
-  margin-top: 1.5rem;
-  margin-bottom: 1.5rem;
+const Title = styled.div<{ $image: string; $isHovered: boolean }>`
+  position: relative;
   width: 100%;
   height: 9rem;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
+  overflow: hidden;
 
   &::after {
     content: '';
@@ -133,6 +153,8 @@ const Title = styled.div<{ $image: string }>`
     background-size: cover;
     background-position: bottom;
     opacity: 0.5;
+    transform: ${({ $isHovered }) => ($isHovered ? 'scale(1.1)' : 'scale(1)')};
+    transition: transform 0.3s ease-in-out;
   }
 `;
 
@@ -142,7 +164,6 @@ const TitleText = styled.p`
   font-weight: bold;
   color: white;
   text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
-  /* color: ${({ theme }) => theme.colors.text.primary}; */
   opacity: 0.8;
 `;
 
