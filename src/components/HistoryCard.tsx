acb0e18952eb9d04ca4b6c85bbd5aae44d176206
KimRiun <kimriun27@gmail.com>
2025-02-26 18:45:08 +0900

    History 카드 이미지 배경 있도록 수정

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -10,93 +10,155 @@ interface HistoryCardProps {
   content: string;
   award?: string;
   image: string;
+  backgroundColor: string;
 }
 
 export default function HistoryCard({ data }: { data: HistoryCardProps }) {
-  const { year, titleText, content, award, image } = data;
+  const { year, titleText, content, award, image, backgroundColor } = data;
   const [showBack, setShowBack] = useState(false);
   const handleClick = () => setShowBack((prev) => !prev);
 
   return (
-    <Frame $showBack={showBack} onClick={handleClick}>
+    <Frame $showBack={showBack} $backgroundColor={backgroundColor} onClick={handleClick}>
       {!showBack ? (
-        <InnerFrame $src={`/images/${image}`}>
+        <InnerFrame $backgroundColor={backgroundColor}>
           <Year>{year}</Year>
-          <Picture>
-            <PictureText>{titleText}</PictureText>
-          </Picture>
+          <Title $image={`/images/${image}`}>
+            <TitleText>{titleText}</TitleText>
+          </Title>
           <Content>{content}</Content>
           {award && <Award>{award}</Award>}
         </InnerFrame>
       ) : (
-        <BackFrame>
-          <div>유니버스</div>
-          <div>미니네이션</div>
-          <div>토리스토리</div>
+        <BackFrame $image={`/images/${image}`}>
+          <BackContent>
+            <div>
+              <p>클라우드 기반 풀스택 금융 개발자 교육 3~9월 (190시간)</p>
+              <ul>
+                <li>기업 가치 평가 자동 템플릿 'ValueGa'</li>
+                <li>주식 잡기 게임(우수상) 'StockmonWorld'</li>
+              </ul>
+            </div>
+            <div>
+              <p>인턴 10월 (한 달)</p>
+              <ul>
+                <li>사내 인사 평가 프로그램</li>
+                <li>잔돈 투자 SOSOL</li>
+              </ul>
+            </div>
+          </BackContent>
         </BackFrame>
       )}
     </Frame>
   );
 }
 
-const Frame = styled.div<{ $showBack: boolean }>`
-  background-color: ${({ $showBack }) => ($showBack ? `white` : `white`)};
-
+const Frame = styled.div<{ $showBack: boolean; $backgroundColor: string }>`
+  position: relative;
+  background-color: ${({ $showBack, $backgroundColor }) =>
+    $showBack ? $backgroundColor : 'white'};
   border-radius: 0.5rem;
-  width: 12rem;
-  height: 20rem;
+  width: 14rem;
+  height: 24rem;
   padding: 1rem;
   box-shadow: 0px 10px 10px rgba(59, 45, 38, 0.1);
   cursor: pointer;
 `;
 
-const BackFrame = styled.div``;
+const BackFrame = styled.div<{ $image: string }>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  /* padding-left: 20%; */
+  /* align-items: center; */
+  flex-direction: column;
 
-const InnerFrame = styled.div<{ $src: string }>`
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: ${({ $image }) => `url(${$image})`};
+    background-size: cover;
+    background-position: bottom;
+    border-radius: 0.5rem;
+    opacity: 0.1;
+  }
+`;
+
+const BackContent = styled.div`
+  z-index: 1;
+`;
+
+const InnerFrame = styled.div<{ $backgroundColor: string }>`
   width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: ${({ theme }) => theme.colors.gray.lightest};
+  background-color: ${({ $backgroundColor }) => $backgroundColor};
   box-shadow: inset 0px 3px 3px rgba(141, 102, 83, 0.1);
-  background-image: ${({ $src }) => `url(${$src})`};
-  background-size: cover;
+  position: relative;
 `;
 
 const Year = styled.p`
+  width: 100%;
   padding-top: 2rem;
-  color: #91d7ea;
+  color: #89ccd8;
+  font-size: large;
   font-weight: bold;
+  text-align: center;
 `;
 
-const Picture = styled.div`
+const Title = styled.div<{ $image: string }>`
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
   width: 100%;
-  height: 7rem;
+  height: 9rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: ${({ $image }) => `url(${$image})`};
+    background-size: cover;
+    background-position: bottom;
+    opacity: 0.5;
+  }
 `;
 
-const PictureImg = styled.img`
-  width: 5rem;
-  fill: ${({ theme }) => theme.colors.text.secondary};
-`;
-
-const PictureText = styled.p`
+const TitleText = styled.p`
+  z-index: 1;
+  font-size: x-large;
   font-weight: bold;
-  color: ${({ theme }) => theme.colors.text.secondary};
+  color: white;
+  text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+  /* color: ${({ theme }) => theme.colors.text.primary}; */
+  opacity: 0.8;
 `;
 
 const Content = styled.p`
   height: 4rem;
-  font-size: 0.9rem;
-  color: ${({ theme }) => theme.colors.text.secondary};
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-size: medium;
+  color: ${({ theme }) => theme.colors.text.primary};
   word-break: keep-all;
   text-align: center;
 `;
 
 const Award = styled.p`
-  font-size: 0.8rem;
-  color: #717171;
+  font-size: small;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  text-align: center;
 `;
