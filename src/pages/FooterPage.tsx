6e479e496f0a56dc6eca7fc3144bfc29e4a2623f
KimRiun <kimriun27@gmail.com>
2025-03-10 21:56:59 +0900

    feat: 파비콘, 프로젝트 설명, 푸터 정보 수정

--- src/pages/FooterPage.tsx
+++ src/pages/FooterPage.tsx
@@ -3,8 +3,16 @@ import styled from 'styled-components';
 export default function FooterPage() {
   return (
     <FooterPageContainer>
-      <p>010-4508-9842</p>
+      <div>
+        <a href='https://github.com/KimRiun' target='_blank' style={{ paddingRight: '1rem' }}>
+          Github
+        </a>
+        <a href='https://velog.io/@sue1199842/posts' target='_blank'>
+          Blog
+        </a>
+      </div>
       <p>kimriun27@gmail.com</p>
+
       <p>ⓒ 2025.KimRiun All rights reserved.</p>
     </FooterPageContainer>
   );
@@ -13,11 +21,12 @@ export default function FooterPage() {
 const FooterPageContainer = styled.div`
   width: 100%;
   height: fit-content;
-  padding: 2rem 0rem;
+  padding: 3rem 0rem;
   color: ${({ theme }) => theme.colors.gray.default};
   background-color: ${({ theme }) => theme.colors.gray.lightest};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 0.5rem;
 `;
